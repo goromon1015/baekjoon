00a7f06dea50b67d0af7d1c5790c922659d33f38
JI HWAN LEE <157870288+goromon1015@users.noreply.github.com>
2024-06-13 00:22:17 +0900

    [Bronze II] Title: 분해합, Time: 148 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-    
-		// 자릿수의 길이를 알기위해 일단 문자열로 입력받는다.
-		String str_N = in.nextLine();
+	public static void main(String[] args) throws IOException {
  
-		// 해당 문자열의 길이 변수
-		int N_len = str_N.length();
- 
-		// 문자열을 정수(int)로 변환 
-		int N = Integer.parseInt(str_N);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int N = Integer.parseInt(br.readLine());
+        
 		int result = 0;
  
 		
-		// i 는 가능한 최솟값인 N - 9 * N의 각 자릿수부터 시작 
-		for(int i = (N - (N_len * 9)); i < N; i++) {
+		for(int i = 0; i < N; i++) {
 			int number = i;
 			int sum = 0;	// 각 자릿수 합 변수 
 			
@@ -36,5 +31,5 @@ public class Main {
  
 		System.out.println(result);
 	}
- 
 }
+ 
